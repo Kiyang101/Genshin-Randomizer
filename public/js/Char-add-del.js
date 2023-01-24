5798a758f7fe77e57200402d1a5ed89ef835790a
kiyang <kiyang202@gmail.com>
2023-01-24 22:00:26 +0700

    .

--- public/js/Char-add-del.js
+++ public/js/Char-add-del.js
@@ -14,24 +14,17 @@ const boss = document.getElementById("boss")
 const char5 = document.getElementById("5-star")
 const charbutton = document.getElementById("buttonchar")
 const bossbutton = document.getElementById("buttonboss")*/
-
+/*
 function Char() {
-
-
   if (document.getElementById("Character").style.display == "none") {
-    
     document.getElementById("Character").style.display = "";
     document.getElementById("buttonchar").style.background = "DodgerBlue";
     document.getElementById("buttonchar").style.color = "white";
-
-
   } else{
     document.getElementById("Character").style.display = "none";
     document.getElementById("buttonchar").style.background = "white";
     document.getElementById("buttonchar").style.color = "black";
-
   }
-
 }
 
 function Boss() {
@@ -39,12 +32,175 @@ function Boss() {
       document.getElementById("boss").style.display = "";
       document.getElementById("buttonboss").style.background = "DodgerBlue";
       document.getElementById("buttonboss").style.color = "white";
-
-
     } else{
       document.getElementById("boss").style.display = "none";
       document.getElementById("buttonboss").style.background = "white";
       document.getElementById("buttonboss").style.color = "black";
-
     }
 }
+*/
+let Charswitch = true;
+let bossswitch = true;
+
+
+
+
+function Char() {
+  if (bossswitch == true){
+    document.querySelector(".Aether").style.cssText = "display: none;"
+    document.querySelector(".Albedo").style.cssText = "display: none;"
+    document.querySelector(".Alhatham").style.cssText = "display: none;"
+    document.querySelector(".Aloy").style.cssText = "display: none;"
+    document.querySelector(".Amber").style.cssText = "display: none;"
+    document.querySelector(".Arataki_Itto").style.cssText = "display: none;"
+    document.querySelector(".Barbara").style.cssText = "display: none;"
+    document.querySelector(".Beidou").style.cssText = "display: none;"
+    document.querySelector(".Bennett").style.cssText = "display: none;"
+    document.querySelector(".Candace").style.cssText = "display: none;"
+    document.querySelector(".Chongyun").style.cssText = "display: none;"
+    document.querySelector(".Collei").style.cssText = "display: none;"
+    document.querySelector(".Cyno").style.cssText = "display: none;"
+    document.querySelector(".Diluc").style.cssText = "display: none;"
+    document.querySelector(".Diona").style.cssText = "display: none;"
+    document.querySelector(".Dori").style.cssText = "display: none;"
+    document.querySelector(".Eula").style.cssText = "display: none;"
+    document.querySelector(".Faruzan").style.cssText = "display: none;"
+    document.querySelector(".Fischl").style.cssText = "display: none;"
+    document.querySelector(".Ganyu").style.cssText = "display: none;"
+    document.querySelector(".Gorou").style.cssText = "display: none;"
+    document.querySelector(".Hutao").style.cssText = "display: none;"
+    document.querySelector(".Jean").style.cssText = "display: none;"
+    document.querySelector(".Kaeya").style.cssText = "display: none;"
+    document.querySelector(".Ayaka").style.cssText = "display: none;"
+    document.querySelector(".Ayato").style.cssText = "display: none;"
+    document.querySelector(".Kazuha").style.cssText = "display: none;"
+    document.querySelector(".Keqing").style.cssText = "display: none;"
+    document.querySelector(".Klee").style.cssText = "display: none;"
+    document.querySelector(".Kujou_Sara").style.cssText = "display: none;"
+    document.querySelector(".Kuki_Shinobu").style.cssText = "display: none;"
+    document.querySelector(".Layla").style.cssText = "display: none;"
+    document.querySelector(".Lisa").style.cssText = "display: none;"
+    document.querySelector(".Mona").style.cssText = "display: none;"
+    document.querySelector(".Nahida").style.cssText = "display: none;"
+    document.querySelector(".Nilou").style.cssText = "display: none;"
+    document.querySelector(".Ningguang").style.cssText = "display: none;"
+    document.querySelector(".Noel").style.cssText = "display: none;"
+    document.querySelector(".Qiqi").style.cssText = "display: none;"
+    document.querySelector(".Raiden").style.cssText = "display: none;"
+    document.querySelector(".Razor").style.cssText = "display: none;"
+    document.querySelector(".Rosaria").style.cssText = "display: none;"
+    document.querySelector(".Kokomi").style.cssText = "display: none;"
+    document.querySelector(".Sayu").style.cssText = "display: none;"
+    document.querySelector(".Shenhe").style.cssText = "display: none;"
+    document.querySelector(".Heizou").style.cssText = "display: none;"
+    document.querySelector(".Sucrose").style.cssText = "display: none;"
+    document.querySelector(".Tartaglia").style.cssText = "display: none;"
+    document.querySelector(".Thoma").style.cssText = "display: none;"
+    document.querySelector(".Tighnari").style.cssText = "display: none;"
+    document.querySelector(".Venti").style.cssText = "display: none;"
+    document.querySelector(".Wanderer").style.cssText = "display: none;"
+    document.querySelector(".Xiangling").style.cssText = "display: none;"
+    document.querySelector(".Xiao").style.cssText = "display: none;"
+    document.querySelector(".Xingqiu").style.cssText = "display: none;"
+    document.querySelector(".Xinyan").style.cssText = "display: none;"
+    document.querySelector(".Yae_miko").style.cssText = "display: none;"
+    document.querySelector(".Yanfei").style.cssText = "display: none;"
+    document.querySelector(".Yaoyao").style.cssText = "display: none;"
+    document.querySelector(".Yelan").style.cssText = "display: none;"
+    document.querySelector(".Yoimiya").style.cssText = "display: none;"
+    document.querySelector(".Yunjin").style.cssText = "display: none;"
+    document.querySelector(".Zhongli").style.cssText = "display: none;"
+    document.getElementById("buttonchar").style.background = "white";
+    document.getElementById("buttonchar").style.color = "black";
+    bossswitch = false;
+    return
+  }
+  if (bossswitch == false){
+    document.querySelector(".Aether").style.cssText = "display: ;"
+    document.querySelector(".Albedo").style.cssText = "display: ;"
+    document.querySelector(".Alhatham").style.cssText = "display: ;"
+    document.querySelector(".Aloy").style.cssText = "display: ;"
+    document.querySelector(".Amber").style.cssText = "display: ;"
+    document.querySelector(".Arataki_Itto").style.cssText = "display: ;"
+    document.querySelector(".Barbara").style.cssText = "display: ;"
+    document.querySelector(".Beidou").style.cssText = "display: ;"
+    document.querySelector(".Bennett").style.cssText = "display: ;"
+    document.querySelector(".Candace").style.cssText = "display: ;"
+    document.querySelector(".Chongyun").style.cssText = "display: ;"
+    document.querySelector(".Collei").style.cssText = "display: ;"
+    document.querySelector(".Cyno").style.cssText = "display: ;"
+    document.querySelector(".Diluc").style.cssText = "display: ;"
+    document.querySelector(".Diona").style.cssText = "display: ;"
+    document.querySelector(".Dori").style.cssText = "display: ;"
+    document.querySelector(".Eula").style.cssText = "display: ;"
+    document.querySelector(".Faruzan").style.cssText = "display: ;"
+    document.querySelector(".Fischl").style.cssText = "display: ;"
+    document.querySelector(".Ganyu").style.cssText = "display: ;"
+    document.querySelector(".Gorou").style.cssText = "display: ;"
+    document.querySelector(".Hutao").style.cssText = "display: ;"
+    document.querySelector(".Jean").style.cssText = "display: ;"
+    document.querySelector(".Kaeya").style.cssText = "display: ;"
+    document.querySelector(".Ayaka").style.cssText = "display: ;"
+    document.querySelector(".Ayato").style.cssText = "display: ;"
+    document.querySelector(".Kazuha").style.cssText = "display: ;"
+    document.querySelector(".Keqing").style.cssText = "display: ;"
+    document.querySelector(".Klee").style.cssText = "display: ;"
+    document.querySelector(".Kujou_Sara").style.cssText = "display: ;"
+    document.querySelector(".Kuki_Shinobu").style.cssText = "display: ;"
+    document.querySelector(".Layla").style.cssText = "display: ;"
+    document.querySelector(".Lisa").style.cssText = "display: ;"
+    document.querySelector(".Mona").style.cssText = "display: ;"
+    document.querySelector(".Nahida").style.cssText = "display: ;"
+    document.querySelector(".Nilou").style.cssText = "display: ;"
+    document.querySelector(".Ningguang").style.cssText = "display: ;"
+    document.querySelector(".Noel").style.cssText = "display: ;"
+    document.querySelector(".Qiqi").style.cssText = "display: ;"
+    document.querySelector(".Raiden").style.cssText = "display: ;"
+    document.querySelector(".Razor").style.cssText = "display: ;"
+    document.querySelector(".Rosaria").style.cssText = "display: ;"
+    document.querySelector(".Kokomi").style.cssText = "display: ;"
+    document.querySelector(".Sayu").style.cssText = "display: ;"
+    document.querySelector(".Shenhe").style.cssText = "display: ;"
+    document.querySelector(".Heizou").style.cssText = "display: ;"
+    document.querySelector(".Sucrose").style.cssText = "display: ;"
+    document.querySelector(".Tartaglia").style.cssText = "display: ;"
+    document.querySelector(".Thoma").style.cssText = "display: ;"
+    document.querySelector(".Tighnari").style.cssText = "display: ;"
+    document.querySelector(".Venti").style.cssText = "display: ;"
+    document.querySelector(".Wanderer").style.cssText = "display: ;"
+    document.querySelector(".Xiangling").style.cssText = "display: ;"
+    document.querySelector(".Xiao").style.cssText = "display: ;"
+    document.querySelector(".Xingqiu").style.cssText = "display: ;"
+    document.querySelector(".Xinyan").style.cssText = "display: ;"
+    document.querySelector(".Yae_miko").style.cssText = "display: ;"
+    document.querySelector(".Yanfei").style.cssText = "display: ;"
+    document.querySelector(".Yaoyao").style.cssText = "display: ;"
+    document.querySelector(".Yelan").style.cssText = "display: ;"
+    document.querySelector(".Yoimiya").style.cssText = "display: ;"
+    document.querySelector(".Yunjin").style.cssText = "display: ;"
+    document.querySelector(".Zhongli").style.cssText = "display: ;"
+    document.getElementById("buttonchar").style.background = "DodgerBlue";
+    document.getElementById("buttonchar").style.color = "white";
+    bossswitch = true;
+    }
+}
+
+
+
+function Boss() {
+  if (Charswitch == true){
+    document.querySelector(".Maguu_Kenki").style.cssText = "display: none;"
+    document.getElementById("buttonboss").style.background = "white";
+    document.getElementById("buttonboss").style.color = "black";
+    Charswitch = false;
+    return
+  }
+  if (Charswitch == false){
+    document.querySelector(".Maguu_Kenki").style.cssText = "display: ;"
+      document.getElementById("buttonboss").style.background = "DodgerBlue";
+      document.getElementById("buttonboss").style.color = "white";
+      Charswitch = true;
+    }
+}
+
+
---
+++ public/js/Select_Character.js
@@ -0,0 +1,113 @@
+const Borders = "border-top: 5px solid lightgreen; border-bottom: 5px solid lightgreen;";
+let count_Aether = true;
+let count_Albedo = true;
+let count_Alhatham = true;
+let count_Aloy = true;
+let count_Amber = true;
+let count_Arataki_Itto = true;
+let count_Barbara = true;
+let count_Beidou = true;
+let count_Bennett = true;
+let count_Candace = true;
+let count_Chongyun = true;
+let count_Collei = true;
+let count_Cyno = true;
+let count_Diluc = true;
+let count_Diona = true;
+let count_Dori = true;
+let count_Eula = true;
+let count_Faruzan = true;
+let count_Fischl = true;
+let count_Ganyu = true;
+let count_Gorou = true;
+let count_Hutao = true;
+let count_Jean = true;
+let count_Kaeya = true;
+let count_Ayaka = true;
+let count_Ayato = true;
+let count_Kazuha = true;
+let count_Keqing = true;
+let count_Klee = true;
+let count_Kujou_Sara = true;
+let count_Kuki_Shinobu = true;
+let count_Layla = true;
+let count_Lisa = true;
+let count_Mona = true;
+let count_Nahida = true;
+let count_Nilou = true;
+let count_Ningguang = true;
+let count_Noel = true;
+let count_Qiqi = true;
+let count_Raiden = true;
+let count_Razor = true;
+let count_Rosaria = true;
+let count_Kokomi = true;
+let count_Sayu = true;
+let count_Shenhe = true;
+let count_Heizou = true;
+let count_Sucrose = true;
+let count_Tartaglia = true;
+let count_Thoma = true;
+let count_Tighnari = true;
+let count_Venti = true;
+let count_Wanderer = true;
+let count_Xiangling = true;
+let count_Xiao = true;
+let count_Xingqiu = true;
+let count_Xinyan = true;
+let count_Yae_miko = true;
+let count_Yanfei = true;
+let count_Yaoyao = true;
+let count_Yelan = true;
+let count_Yoimiya = true;
+let count_Yunjin = true;
+let count_Zhongli = true;
+
+
+function select_Aether() {
+  if (count_Aether == true){
+    document.querySelector(".char .Aether .box").style.cssText = Borders
+    count_Aether = false;
+    return
+  }
+  if (count_Aether == false){
+    document.querySelector(".char .Aether .box").style.border="";
+    count_Aether = true;
+  }
+}
+
+function select_Albedo() {
+    if (count_Albedo == true){
+      document.querySelector(".char .Albedo .box").style.cssText = Borders
+      count_Albedo = false;
+      return
+    }
+    if (count_Albedo == false){
+      document.querySelector(".char .Albedo .box").style.border="";
+      count_Albedo = true;
+    }
+}
+
+function select_Alhatham() {
+  if (count_Alhatham == true){
+    document.querySelector(".char .Alhatham .box").style.cssText = Borders
+    count_Alhatham = false;
+    return
+  }
+  if (count_Alhatham == false){
+    document.querySelector(".char .Alhatham .box").style.border="";
+    count_Alhatham = true;
+  }
+}
+
+function select_Aloy() {
+  if (count_Aloy == true){
+    document.querySelector(".char .Aloy .box").style.cssText = Borders
+    count_Aloy = false;
+    return
+  }
+  if (count_Aloy == false){
+    document.querySelector(".char .Aloy .box").style.border="";
+    count_Aloy = true;
+  }
+}
