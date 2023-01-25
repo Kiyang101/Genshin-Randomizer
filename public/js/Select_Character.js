f0b7509355a1fea8619f7a278ee4296afab0e514
kiyang <kiyang202@gmail.com>
2023-01-25 20:54:20 +0700

    .

--- public/js/Select_Character.js
+++ public/js/Select_Character.js
@@ -111,3 +111,723 @@ function select_Aloy() {
     count_Aloy = true;
   }
 }
+
+function select_Amber() {
+  if (count_Amber == true){
+    document.querySelector(".char .Amber .box").style.cssText = Borders
+    count_Amber = false;
+    return
+  }
+  if (count_Amber == false){
+    document.querySelector(".char .Amber .box").style.border="";
+    count_Amber = true;
+  }
+}
+
+function select_Arataki_Itto() {
+  if (count_Arataki_Itto == true){
+    document.querySelector(".char .Arataki_Itto .box").style.cssText = Borders
+    count_Arataki_Itto = false;
+    return
+  }
+  if (count_Arataki_Itto == false){
+    document.querySelector(".char .Arataki_Itto .box").style.border="";
+    count_Arataki_Itto = true;
+  }
+}
+
+function select_Barbara() {
+  if (count_Barbara == true){
+    document.querySelector(".char .Barbara .box").style.cssText = Borders
+    count_Barbara = false;
+    return
+  }
+  if (count_Barbara == false){
+    document.querySelector(".char .Barbara .box").style.border="";
+    count_Barbara = true;
+  }
+}
+
+function select_Beidou() {
+  if (count_Beidou == true){
+    document.querySelector(".char .Beidou .box").style.cssText = Borders
+    count_Beidou = false;
+    return
+  }
+  if (count_Beidou == false){
+    document.querySelector(".char .Beidou .box").style.border="";
+    count_Beidou = true;
+  }
+}
+
+function select_Bennett() {
+  if (count_Bennett == true){
+    document.querySelector(".char .Bennett .box").style.cssText = Borders
+    count_Bennett = false;
+    return
+  }
+  if (count_Bennett == false){
+    document.querySelector(".char .Bennett .box").style.border="";
+    count_Bennett = true;
+  }
+}
+
+function select_Candace() {
+  if (count_Candace == true){
+    document.querySelector(".char .Candace .box").style.cssText = Borders
+    count_Candace = false;
+    return
+  }
+  if (count_Candace == false){
+    document.querySelector(".char .Candace .box").style.border="";
+    count_Candace = true;
+  }
+}
+
+function select_Chongyun() {
+  if (count_Chongyun == true){
+    document.querySelector(".char .Chongyun .box").style.cssText = Borders
+    count_Chongyun = false;
+    return
+  }
+  if (count_Chongyun == false){
+    document.querySelector(".char .Chongyun .box").style.border="";
+    count_Chongyun = true;
+  }
+}
+
+function select_Collei() {
+  if (count_Collei == true){
+    document.querySelector(".char .Collei .box").style.cssText = Borders
+    count_Collei = false;
+    return
+  }
+  if (count_Collei == false){
+    document.querySelector(".char .Collei .box").style.border="";
+    count_Collei = true;
+  }
+}
+
+function select_Cyno() {
+  if (count_Cyno == true){
+    document.querySelector(".char .Cyno .box").style.cssText = Borders
+    count_Cyno = false;
+    return
+  }
+  if (count_Cyno == false){
+    document.querySelector(".char .Cyno .box").style.border="";
+    count_Cyno = true;
+  }
+}
+
+function select_Diluc() {
+  if (count_Diluc == true){
+    document.querySelector(".char .Diluc .box").style.cssText = Borders
+    count_Diluc = false;
+    return
+  }
+  if (count_Diluc == false){
+    document.querySelector(".char .Diluc .box").style.border="";
+    count_Diluc = true;
+  }
+}
+
+function select_Diona() {
+  if (count_Diona == true){
+    document.querySelector(".char .Diona .box").style.cssText = Borders
+    count_Diona = false;
+    return
+  }
+  if (count_Diona == false){
+    document.querySelector(".char .Diona .box").style.border="";
+    count_Diona = true;
+  }
+}
+
+function select_Dori() {
+  if (count_Dori == true){
+    document.querySelector(".char .Dori .box").style.cssText = Borders
+    count_Dori = false;
+    return
+  }
+  if (count_Dori == false){
+    document.querySelector(".char .Dori .box").style.border="";
+    count_Dori = true;
+  }
+}
+
+function select_Eula() {
+  if (count_Eula == true){
+    document.querySelector(".char .Eula .box").style.cssText = Borders
+    count_Eula = false;
+    return
+  }
+  if (count_Eula == false){
+    document.querySelector(".char .Eula .box").style.border="";
+    count_Eula = true;
+  }
+}
+
+function select_Faruzan() {
+  if (count_Faruzan == true){
+    document.querySelector(".char .Faruzan .box").style.cssText = Borders
+    count_Faruzan = false;
+    return
+  }
+  if (count_Faruzan == false){
+    document.querySelector(".char .Faruzan .box").style.border="";
+    count_Faruzan = true;
+  }
+}
+
+function select_Fischl() {
+  if (count_Fischl == true){
+    document.querySelector(".char .Fischl .box").style.cssText = Borders
+    count_Fischl = false;
+    return
+  }
+  if (count_Fischl == false){
+    document.querySelector(".char .Fischl .box").style.border="";
+    count_Fischl = true;
+  }
+}
+
+function select_Ganyu() {
+  if (count_Ganyu == true){
+    document.querySelector(".char .Ganyu .box").style.cssText = Borders
+    count_Ganyu = false;
+    return
+  }
+  if (count_Ganyu == false){
+    document.querySelector(".char .Ganyu .box").style.border="";
+    count_Ganyu = true;
+  }
+}
+
+function select_Ganyu() {
+  if (count_Ganyu == true){
+    document.querySelector(".char .Ganyu .box").style.cssText = Borders
+    count_Ganyu = false;
+    return
+  }
+  if (count_Ganyu == false){
+    document.querySelector(".char .Ganyu .box").style.border="";
+    count_Ganyu = true;
+  }
+}
+
+function select_Gorou() {
+  if (count_Gorou == true){
+    document.querySelector(".char .Gorou .box").style.cssText = Borders
+    count_Gorou = false;
+    return
+  }
+  if (count_Gorou == false){
+    document.querySelector(".char .Gorou .box").style.border="";
+    count_Gorou = true;
+  }
+}
+
+function select_Hutao() {
+  if (count_Hutao == true){
+    document.querySelector(".char .Hutao .box").style.cssText = Borders
+    count_Hutao = false;
+    return
+  }
+  if (count_Hutao == false){
+    document.querySelector(".char .Hutao .box").style.border="";
+    count_Hutao = true;
+  }
+}
+
+function select_Jean() {
+  if (count_Jean == true){
+    document.querySelector(".char .Jean .box").style.cssText = Borders
+    count_Jean = false;
+    return
+  }
+  if (count_Jean == false){
+    document.querySelector(".char .Jean .box").style.border="";
+    count_Jean = true;
+  }
+}
+
+function select_Kaeya() {
+  if (count_Kaeya == true){
+    document.querySelector(".char .Kaeya .box").style.cssText = Borders
+    count_Kaeya = false;
+    return
+  }
+  if (count_Kaeya == false){
+    document.querySelector(".char .Kaeya .box").style.border="";
+    count_Kaeya = true;
+  }
+}
+
+function select_Ayaka() {
+  if (count_Ayaka == true){
+    document.querySelector(".char .Ayaka .box").style.cssText = Borders
+    count_Ayaka = false;
+    return
+  }
+  if (count_Ayaka == false){
+    document.querySelector(".char .Ayaka .box").style.border="";
+    count_Ayaka = true;
+  }
+}
+
+function select_Ayato() {
+  if (count_Ayato == true){
+    document.querySelector(".char .Ayato .box").style.cssText = Borders
+    count_Ayato = false;
+    return
+  }
+  if (count_Ayato == false){
+    document.querySelector(".char .Ayato .box").style.border="";
+    count_Ayato = true;
+  }
+}
+
+function select_Kazuha() {
+  if (count_Kazuha == true){
+    document.querySelector(".char .Kazuha .box").style.cssText = Borders
+    count_Kazuha = false;
+    return
+  }
+  if (count_Kazuha == false){
+    document.querySelector(".char .Kazuha .box").style.border="";
+    count_Kazuha = true;
+  }
+}
+
+function select_Keqing() {
+  if (count_Keqing == true){
+    document.querySelector(".char .Keqing .box").style.cssText = Borders
+    count_Keqing = false;
+    return
+  }
+  if (count_Keqing == false){
+    document.querySelector(".char .Keqing .box").style.border="";
+    count_Keqing = true;
+  }
+}
+
+function select_Klee() {
+  if (count_Klee == true){
+    document.querySelector(".char .Klee .box").style.cssText = Borders
+    count_Klee = false;
+    return
+  }
+  if (count_Klee == false){
+    document.querySelector(".char .Klee .box").style.border="";
+    count_Klee = true;
+  }
+}
+
+function select_Kujou_Sara() {
+  if (count_Kujou_Sara == true){
+    document.querySelector(".char .Kujou_Sara .box").style.cssText = Borders
+    count_Kujou_Sara = false;
+    return
+  }
+  if (count_Kujou_Sara == false){
+    document.querySelector(".char .Kujou_Sara .box").style.border="";
+    count_Kujou_Sara = true;
+  }
+}
+
+function select_Kuki_Shinobu() {
+  if (count_Kuki_Shinobu == true){
+    document.querySelector(".char .Kuki_Shinobu .box").style.cssText = Borders
+    count_Kuki_Shinobu = false;
+    return
+  }
+  if (count_Kuki_Shinobu == false){
+    document.querySelector(".char .Kuki_Shinobu .box").style.border="";
+    count_Kuki_Shinobu = true;
+  }
+}
+
+function select_Layla() {
+  if (count_Layla == true){
+    document.querySelector(".char .Layla .box").style.cssText = Borders
+    count_Layla = false;
+    return
+  }
+  if (count_Layla == false){
+    document.querySelector(".char .Layla .box").style.border="";
+    count_Layla = true;
+  }
+}
+
+function select_Lisa() {
+  if (count_Lisa == true){
+    document.querySelector(".char .Lisa .box").style.cssText = Borders
+    count_Lisa = false;
+    return
+  }
+  if (count_Lisa == false){
+    document.querySelector(".char .Lisa .box").style.border="";
+    count_Lisa = true;
+  }
+}
+
+function select_Mona() {
+  if (count_Mona == true){
+    document.querySelector(".char .Mona .box").style.cssText = Borders
+    count_Mona = false;
+    return
+  }
+  if (count_Mona == false){
+    document.querySelector(".char .Mona .box").style.border="";
+    count_Mona = true;
+  }
+}
+
+function select_Nahida() {
+  if (count_Nahida == true){
+    document.querySelector(".char .Nahida .box").style.cssText = Borders
+    count_Nahida = false;
+    return
+  }
+  if (count_Nahida == false){
+    document.querySelector(".char .Nahida .box").style.border="";
+    count_Nahida = true;
+  }
+}
+
+function select_Nilou() {
+  if (count_Nilou == true){
+    document.querySelector(".char .Nilou .box").style.cssText = Borders
+    count_Nilou = false;
+    return
+  }
+  if (count_Nilou == false){
+    document.querySelector(".char .Nilou .box").style.border="";
+    count_Nilou = true;
+  }
+}
+
+function select_Ningguang() {
+  if (count_Ningguang == true){
+    document.querySelector(".char .Ningguang .box").style.cssText = Borders
+    count_Ningguang = false;
+    return
+  }
+  if (count_Ningguang == false){
+    document.querySelector(".char .Ningguang .box").style.border="";
+    count_Ningguang = true;
+  }
+}
+
+function select_Noel() {
+  if (count_Noel == true){
+    document.querySelector(".char .Noel .box").style.cssText = Borders
+    count_Noel = false;
+    return
+  }
+  if (count_Noel == false){
+    document.querySelector(".char .Noel .box").style.border="";
+    count_Noel = true;
+  }
+}
+
+function select_Qiqi() {
+  if (count_Qiqi == true){
+    document.querySelector(".char .Qiqi .box").style.cssText = Borders
+    count_Qiqi = false;
+    return
+  }
+  if (count_Qiqi == false){
+    document.querySelector(".char .Qiqi .box").style.border="";
+    count_Qiqi = true;
+  }
+}
+
+function select_Raiden() {
+  if (count_Raiden == true){
+    document.querySelector(".char .Raiden .box").style.cssText = Borders
+    count_Raiden = false;
+    return
+  }
+  if (count_Raiden == false){
+    document.querySelector(".char .Raiden .box").style.border="";
+    count_Raiden = true;
+  }
+}
+
+function select_Razor() {
+  if (count_Razor == true){
+    document.querySelector(".char .Razor .box").style.cssText = Borders
+    count_Razor = false;
+    return
+  }
+  if (count_Razor == false){
+    document.querySelector(".char .Razor .box").style.border="";
+    count_Razor = true;
+  }
+}
+
+function select_Rosaria() {
+  if (count_Rosaria == true){
+    document.querySelector(".char .Rosaria .box").style.cssText = Borders
+    count_Rosaria = false;
+    return
+  }
+  if (count_Rosaria == false){
+    document.querySelector(".char .Rosaria .box").style.border="";
+    count_Rosaria = true;
+  }
+}
+
+function select_Kokomi() {
+  if (count_Kokomi == true){
+    document.querySelector(".char .Kokomi .box").style.cssText = Borders
+    count_Kokomi = false;
+    return
+  }
+  if (count_Kokomi == false){
+    document.querySelector(".char .Kokomi .box").style.border="";
+    count_Kokomi = true;
+  }
+}
+
+function select_Sayu() {
+  if (count_Sayu == true){
+    document.querySelector(".char .Sayu .box").style.cssText = Borders
+    count_Sayu = false;
+    return
+  }
+  if (count_Sayu == false){
+    document.querySelector(".char .Sayu .box").style.border="";
+    count_Sayu = true;
+  }
+}
+
+function select_Shenhe() {
+  if (count_Shenhe == true){
+    document.querySelector(".char .Shenhe .box").style.cssText = Borders
+    count_Shenhe = false;
+    return
+  }
+  if (count_Shenhe == false){
+    document.querySelector(".char .Shenhe .box").style.border="";
+    count_Shenhe = true;
+  }
+}
+
+function select_Heizou() {
+  if (count_Heizou == true){
+    document.querySelector(".char .Heizou .box").style.cssText = Borders
+    count_Heizou = false;
+    return
+  }
+  if (count_Heizou == false){
+    document.querySelector(".char .Heizou .box").style.border="";
+    count_Heizou = true;
+  }
+}
+
+function select_Sucrose() {
+  if (count_Sucrose == true){
+    document.querySelector(".char .Sucrose .box").style.cssText = Borders
+    count_Sucrose = false;
+    return
+  }
+  if (count_Sucrose == false){
+    document.querySelector(".char .Sucrose .box").style.border="";
+    count_Sucrose = true;
+  }
+}
+
+function select_Tartaglia() {
+  if (count_Tartaglia == true){
+    document.querySelector(".char .Tartaglia .box").style.cssText = Borders
+    count_Tartaglia = false;
+    return
+  }
+  if (count_Tartaglia == false){
+    document.querySelector(".char .Tartaglia .box").style.border="";
+    count_Tartaglia = true;
+  }
+}
+
+function select_Thoma() {
+  if (count_Thoma == true){
+    document.querySelector(".char .Thoma .box").style.cssText = Borders
+    count_Thoma = false;
+    return
+  }
+  if (count_Thoma == false){
+    document.querySelector(".char .Thoma .box").style.border="";
+    count_Thoma = true;
+  }
+}
+
+function select_Tighnari() {
+  if (count_Tighnari == true){
+    document.querySelector(".char .Tighnari .box").style.cssText = Borders
+    count_Tighnari = false;
+    return
+  }
+  if (count_Tighnari == false){
+    document.querySelector(".char .Tighnari .box").style.border="";
+    count_Tighnari = true;
+  }
+}
+
+function select_Venti() {
+  if (count_Venti == true){
+    document.querySelector(".char .Venti .box").style.cssText = Borders
+    count_Venti = false;
+    return
+  }
+  if (count_Venti == false){
+    document.querySelector(".char .Venti .box").style.border="";
+    count_Venti = true;
+  }
+}
+
+function select_Wanderer() {
+  if (count_Wanderer == true){
+    document.querySelector(".char .Wanderer .box").style.cssText = Borders
+    count_Wanderer = false;
+    return
+  }
+  if (count_Wanderer == false){
+    document.querySelector(".char .Wanderer .box").style.border="";
+    count_Wanderer = true;
+  }
+}
+
+function select_Xiangling() {
+  if (count_Xiangling == true){
+    document.querySelector(".char .Xiangling .box").style.cssText = Borders
+    count_Xiangling = false;
+    return
+  }
+  if (count_Xiangling == false){
+    document.querySelector(".char .Xiangling .box").style.border="";
+    count_Xiangling = true;
+  }
+}
+
+function select_Xiao() {
+  if (count_Xiao == true){
+    document.querySelector(".char .Xiao .box").style.cssText = Borders
+    count_Xiao = false;
+    return
+  }
+  if (count_Xiao == false){
+    document.querySelector(".char .Xiao .box").style.border="";
+    count_Xiao = true;
+  }
+}
+
+function select_Xingqiu() {
+  if (count_Xingqiu == true){
+    document.querySelector(".char .Xingqiu .box").style.cssText = Borders
+    count_Xingqiu = false;
+    return
+  }
+  if (count_Xingqiu == false){
+    document.querySelector(".char .Xingqiu .box").style.border="";
+    count_Xingqiu = true;
+  }
+}
+
+function select_Xinyan() {
+  if (count_Xinyan == true){
+    document.querySelector(".char .Xinyan .box").style.cssText = Borders
+    count_Xinyan = false;
+    return
+  }
+  if (count_Xinyan == false){
+    document.querySelector(".char .Xinyan .box").style.border="";
+    count_Xinyan = true;
+  }
+}
+
+function select_Yae_miko() {
+  if (count_Yae_miko == true){
+    document.querySelector(".char .Yae_miko .box").style.cssText = Borders
+    count_Yae_miko = false;
+    return
+  }
+  if (count_Yae_miko == false){
+    document.querySelector(".char .Yae_miko .box").style.border="";
+    count_Yae_miko = true;
+  }
+}
+
+function select_Yanfei() {
+  if (count_Yanfei == true){
+    document.querySelector(".char .Yanfei .box").style.cssText = Borders
+    count_Yanfei = false;
+    return
+  }
+  if (count_Yanfei == false){
+    document.querySelector(".char .Yanfei .box").style.border="";
+    count_Yanfei = true;
+  }
+}
+
+function select_Yaoyao() {
+  if (count_Yaoyao == true){
+    document.querySelector(".char .Yaoyao .box").style.cssText = Borders
+    count_Yaoyao = false;
+    return
+  }
+  if (count_Yaoyao == false){
+    document.querySelector(".char .Yaoyao .box").style.border="";
+    count_Yaoyao = true;
+  }
+}
+
+function select_Yelan() {
+  if (count_Yelan == true){
+    document.querySelector(".char .Yelan .box").style.cssText = Borders
+    count_Yelan = false;
+    return
+  }
+  if (count_Yelan == false){
+    document.querySelector(".char .Yelan .box").style.border="";
+    count_Yelan = true;
+  }
+}
+
+function select_Yoimiya() {
+  if (count_Yoimiya == true){
+    document.querySelector(".char .Yoimiya .box").style.cssText = Borders
+    count_Yoimiya = false;
+    return
+  }
+  if (count_Yoimiya == false){
+    document.querySelector(".char .Yoimiya .box").style.border="";
+    count_Yoimiya = true;
+  }
+}
+
+function select_Yunjin() {
+  if (count_Yunjin == true){
+    document.querySelector(".char .Yunjin .box").style.cssText = Borders
+    count_Yunjin = false;
+    return
+  }
+  if (count_Yunjin == false){
+    document.querySelector(".char .Yunjin .box").style.border="";
+    count_Yunjin = true;
+  }
+}
+
+function select_Zhongli() {
+  if (count_Zhongli == true){
+    document.querySelector(".char .Zhongli .box").style.cssText = Borders
+    count_Zhongli = false;
+    return
+  }
+  if (count_Zhongli == false){
+    document.querySelector(".char .Zhongli .box").style.border="";
+    count_Zhongli = true;
+  }
+}
