8889d21f4607c2cbaee3f344f4eb98f5ad63d0bb
kiyang <kiyang202@gmail.com>
2023-02-05 17:16:49 +0700

    .

--- public/js/display.js
+++ public/js/display.js
@@ -1,5 +1,6 @@
 let csw = true;
 let bsw = true;
+let ransw = false;
 
 function dischar(){
     if (csw==true){
@@ -33,3 +34,70 @@ function disboss(){
     }
 }
 
+function Random(){
+
+    if (ransw == false){
+        document.querySelector("div.Random").style.cssText = "display: ;"
+        document.querySelector(".char-caret-right").style.cssText = "display: ;"
+        document.querySelector(".char-caret-down").style.cssText = "display: none;"
+        document.querySelector("div.char").style.cssText = "display: none;"
+        document.querySelector(".boss-caret-right").style.cssText = "display: ;"
+        document.querySelector(".boss-caret-down").style.cssText = "display: none;"
+        document.querySelector("div.Boss").style.cssText = "display: none;"
+        document.querySelector(".displaycharacter").style.cssText = "pointer-events: none;"
+        document.querySelector(".displayboss").style.cssText = "pointer-events: none;"
+        document.querySelector(".filter").style.cssText = "pointer-events: none;"
+
+
+        csw = false;
+        bsw = false;
+        ransw = true;
+        return
+    }
+
+    if (ransw == true){
+        document.querySelector("div.Random").style.cssText = "display: none;"
+        document.querySelector(".char-caret-right").style.cssText = "display: none;"
+        document.querySelector(".char-caret-down").style.cssText = "display: ;"
+        document.querySelector("div.char").style.cssText = "display: ;"
+        document.querySelector(".boss-caret-right").style.cssText = "display: none;"
+        document.querySelector(".boss-caret-down").style.cssText = "display: ;"
+        document.querySelector("div.Boss").style.cssText = "display: ;"
+        document.querySelector(".displaycharacter").style.cssText = "pointer-events: visible;"
+        document.querySelector(".displayboss").style.cssText = "pointer-events: visible;"
+        document.querySelector(".filter").style.cssText = "pointer-events: visible;"
+
+
+        csw = true;
+        bsw = true;
+        ransw = false;
+        return
+    }
+}
+
+function start_random(){
+    document.querySelector(".start_random").style.cssText = "display: none;"
+    document.querySelector(".section_random").style.cssText = "display: ;"
+    document.querySelector(".return").style.cssText = "display: ;"
+    document.querySelector(".random").style.cssText = "pointer-events: none;"
+}
+
+function Return(){
+    document.querySelector("div.Random").style.cssText = "display: none;"
+    document.querySelector(".char-caret-right").style.cssText = "display: none;"
+    document.querySelector(".char-caret-down").style.cssText = "display: ;"
+    document.querySelector("div.char").style.cssText = "display: ;"
+    document.querySelector(".boss-caret-right").style.cssText = "display: none;"
+    document.querySelector(".boss-caret-down").style.cssText = "display: ;"
+    document.querySelector("div.Boss").style.cssText = "display: ;"
+    document.querySelector(".displaycharacter").style.cssText = "pointer-events: visible;"
+    document.querySelector(".displayboss").style.cssText = "pointer-events: visible;"
+    document.querySelector(".filter").style.cssText = "pointer-events: visible;"
+    document.querySelector(".random").style.cssText = "pointer-events: visible;"
+    document.querySelector(".section_random").style.cssText = "display: none;"
+    document.querySelector(".start_random").style.cssText = "display: ;"
+    document.querySelector(".return").style.cssText = "display: none;"
+
+    ransw = false;
+
+}
